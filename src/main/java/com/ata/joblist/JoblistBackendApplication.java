367661eb3bec2e4222f35e731a556724245a0604
PaingPyae <paingpyae.dev@gmail.com>
2024-07-05 07:36:07 +0700

    Create Project

---
+++ src/main/java/com/ata/joblist/JoblistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ata.joblist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoblistBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ata/joblist/JoblistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ata.joblist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
